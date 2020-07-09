3330e64cdba5802dd117ddf4a976373c1cabb67e
DasGeek <36272016+dasgeekchannel@users.noreply.github.com>
2020-07-09 01:35:48 -0400

    Add files via upload
    Userinput shows basic error handling for user input

---
+++ userinput.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name: ")
+age = int(input("Enter your age: "))
+hundred = 100 - age
+print("Happy to me you",name,age)
+print("You will be 100 years old in", hundred, "years")
