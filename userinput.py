64eb07d6714508cffb7f764d97b83412f4f922a1
DasGeek <36272016+dasgeekchannel@users.noreply.github.com>
2020-07-09 01:36:37 -0400

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -1,5 +1,10 @@
-name = input("Enter Your Name: ")
-age = int(input("Enter your age: "))
-hundred = 100 - age
-print("Happy to me you",name,age)
-print("You will be 100 years old in", hundred, "years")
+while True:
+    try:
+        cinput = input("Enter your name: ")
+        dinput = int(input("Enter your age: "))
+    except ValueError:
+        print("You need to enter a real number.")
+    else:
+        calc_inp = 100 - dinput
+        print("Your name is,", cinput, "you are,", dinput, "years old", "you will be one hundred years old in just", calc_inp, "years!")
+        break
